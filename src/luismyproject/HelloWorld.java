5041674780a04db51828f334690cc20a05092d0e
ibhiabol <luis.ibhiabor@oup.com>
2016-04-27 09:08:14 +0100

    This is an introduction course for using eclipse and github.

---
+++ src/luismyproject/HelloWorld.java
@@ -0,0 +1,10 @@
+package luismyproject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! Welcome to the world of Github and Eclipse.");
+
+	}
+
+}
